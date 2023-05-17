9db0a981acec8ff8bf8d987378ccfba027a108e9
Anastasia <anastasya14022015@gmail.com>
2023-05-17 22:15:54 +0300

    task2.3

--- task_2/solution.js
+++ task_2/solution.js
@@ -62,17 +62,21 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
-
+    let totalSum;
     // присвойте totalSum значение productsSum
+    let totalSum = productsSum;
     // уменьшите totalSum на discountSum
+    totalSum = (totalSum - discountSum);
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum = (totalSum + shippingSum);
     // создайте переменную freeShipping
+    let freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    freeShipping = !Boolean(shippingSum);
 
     // Конец решения задачи №2.3.
 
